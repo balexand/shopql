482ed2ec919753c0d6fdb30aeb314d22a3d3e3d6
Brian Alexander <balexand@gmail.com>
2022-07-28 12:56:26 -0600

    Update integration_test.exs

--- test/shopql/integration_test.exs
+++ test/shopql/integration_test.exs
@@ -12,67 +12,67 @@ defmodule ShopQL.IntegrationTest do
   end
 
   test "request" do
-    result =
-      ShopQL.request(
-        """
-        query($gid: ID!) {
-          product(id: $gid) {
-            variants(first: 5) {
-              pageInfo {
-                hasNextPage
-              },
-              edges {
-                node {
-                  id
-                  sellableOnlineQuantity
-                }
-              }
-            }
-          }
-        }
-        """,
-        %{gid: "gid://shopify/Product/7595694915746"},
-        opts()
-      )
+    assert {:ok, data, extensions} =
+             ShopQL.request(
+               """
+               query($gid: ID!) {
+                 product(id: $gid) {
+                   variants(first: 5) {
+                     pageInfo {
+                       hasNextPage
+                     },
+                     edges {
+                       node {
+                         id
+                         sellableOnlineQuantity
+                       }
+                     }
+                   }
+                 }
+               }
+               """,
+               %{gid: "gid://shopify/Product/7595694915746"},
+               opts()
+             )
 
-    assert {:ok,
-            %{
-              "product" => %{
-                "variants" => %{
-                  "edges" => [
-                    %{
-                      "node" => %{
-                        "id" => "gid://shopify/ProductVariant/42424218845346",
-                        "sellableOnlineQuantity" => 1
-                      }
-                    },
-                    %{
-                      "node" => %{
-                        "id" => "gid://shopify/ProductVariant/42424218878114",
-                        "sellableOnlineQuantity" => 2
-                      }
-                    },
-                    %{
-                      "node" => %{
-                        "id" => "gid://shopify/ProductVariant/42424218910882",
-                        "sellableOnlineQuantity" => 3
-                      }
-                    }
-                  ],
-                  "pageInfo" => %{"hasNextPage" => false}
-                }
-              }
-            },
-            %{
-              "cost" => %{
-                "actualQueryCost" => 6,
-                "requestedQueryCost" => 8,
-                "throttleStatus" => %{
-                  "currentlyAvailable" => _currently_available,
-                  "maximumAvailable" => _maximum_available,
-                  "restoreRate" => 50.0
-                }
-              }
-            }} = result
+    assert data == %{
+             "product" => %{
+               "variants" => %{
+                 "edges" => [
+                   %{
+                     "node" => %{
+                       "id" => "gid://shopify/ProductVariant/42424218845346",
+                       "sellableOnlineQuantity" => 1
+                     }
+                   },
+                   %{
+                     "node" => %{
+                       "id" => "gid://shopify/ProductVariant/42424218878114",
+                       "sellableOnlineQuantity" => 2
+                     }
+                   },
+                   %{
+                     "node" => %{
+                       "id" => "gid://shopify/ProductVariant/42424218910882",
+                       "sellableOnlineQuantity" => 3
+                     }
+                   }
+                 ],
+                 "pageInfo" => %{"hasNextPage" => false}
+               }
+             }
+           }
+
+    assert %{
+             "cost" => %{
+               "actualQueryCost" => 6,
+               "requestedQueryCost" => 8,
+               "throttleStatus" => %{
+                 "currentlyAvailable" => _currently_available,
+                 "maximumAvailable" => _maximum_available,
+                 "restoreRate" => 50.0
+               }
+             }
+           } = extensions
   end
 end
